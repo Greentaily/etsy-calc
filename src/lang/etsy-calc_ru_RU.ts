aace6ef4eab9b893fae40e2b565c88837eb68054
Greentaily <greentail@protonmail.ch>
2020-12-11 16:26:17 +0300

    Update Russian translations

--- src/lang/etsy-calc_ru_RU.ts
+++ src/lang/etsy-calc_ru_RU.ts
@@ -1,120 +1,91 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="etsy-calc_ru_RU">
+<TS version="2.1" language="ru">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="20"/>
+        <location filename="../forms/mainwindow.ui" line="20"/>
         <source>[window_title]</source>
         <extracomment>Title of the window</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Калькулятор Etsy</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="40"/>
+        <location filename="../forms/mainwindow.ui" line="40"/>
         <source>[groupbox_shipping]</source>
         <extracomment>Title of the shipping group box (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Почтовые расходы</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="80"/>
+        <location filename="../forms/mainwindow.ui" line="80"/>
         <source>[shipping_charge]</source>
         <extracomment>Shipping charge label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Цена доставки</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="128"/>
-        <source>Германия</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="133"/>
-        <source>США</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="150"/>
+        <location filename="../forms/mainwindow.ui" line="140"/>
         <source>[shipping_cost]</source>
         <extracomment>Shipping cost label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Стоимость отправки</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="166"/>
+        <location filename="../forms/mainwindow.ui" line="156"/>
         <source>[package_weight]</source>
         <extracomment>Package weight label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Вес посылки</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="182"/>
+        <location filename="../forms/mainwindow.ui" line="172"/>
         <source>[recipient_country]</source>
         <extracomment>Recipient country label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Страна получателя</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="202"/>
-        <source> г</source>
-        <translation type="unfinished"></translation>
+        <location filename="../forms/mainwindow.ui" line="192"/>
+        <source> g</source>
+        <extracomment>Weight unit symbol</extracomment>
+        <translation> г</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="224"/>
-        <source>[fetch_tariffs]</source>
-        <extracomment>&quot;Fetch shipping tariffs&quot; checkbox label (static)</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="248"/>
-        <source>[total_fees]</source>
-        <extracomment>Total fees static label</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="269"/>
+        <location filename="../forms/mainwindow.ui" line="259"/>
         <source>[gross_profit]</source>
         <extracomment>Gross profit static label</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Прибыль</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="315"/>
-        <source>US$</source>
-        <translation type="unfinished"></translation>
+        <location filename="../forms/mainwindow.ui" line="214"/>
+        <source>[use_tariffs]</source>
+        <extracomment>&quot;Use tariffs&quot; checkbox label (static)</extracomment>
+        <translation>По тарифу</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="336"/>
-        <source>Paypal (4.4% + $0.30)</source>
-        <translation type="unfinished"></translation>
+        <location filename="../forms/mainwindow.ui" line="238"/>
+        <source>[expenses]</source>
+        <extracomment>Expenses static label</extracomment>
+        <translation>Расходы</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="341"/>
-        <source>Direct Checkout (3% + $0.25)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="400"/>
+        <location filename="../forms/mainwindow.ui" line="375"/>
         <source>[price]</source>
         <extracomment>Price of the item label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Цена изделия</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="413"/>
+        <location filename="../forms/mainwindow.ui" line="388"/>
         <source>[tax]</source>
         <extracomment>Tax label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Налогооблажение</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="426"/>
+        <location filename="../forms/mainwindow.ui" line="401"/>
         <source>[payment_method]</source>
         <extracomment>Payment method label (static)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Метод оплаты</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="439"/>
+        <location filename="../forms/mainwindow.ui" line="414"/>
         <source>[client_currency]</source>
         <extracomment>Currency of the client label (static)</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="484"/>
-        <source>УСН 6%</source>
-        <translation type="unfinished"></translation>
+        <translation>Валюта клиента</translation>
     </message>
 </context>
 </TS>
